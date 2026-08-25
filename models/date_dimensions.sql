76a356e406f16bed69b072c55175e3bbeea3e42d
Hemachand K <hemachand.katna@nagarro.com>
2026-08-25 12:23:37 +0000

    new model dataset and its changes

--- models/date_dimensions.sql
+++ models/date_dimensions.sql
@@ -1,15 +1,13 @@
 WITH CTE AS (
     select 
-    STARTED_AT,
-    DATE(TO_TIMESTAMP(STARTED_AT)) as DATE_STARTED_AT,
-    DAYNAME(TO_TIMESTAMP(STARTED_AT)) as DAY_NAME,
-    {{day_type('STARTED_AT')}} as DAY_TYPE,
-    MONTHNAME(TO_TIMESTAMP(STARTED_AT)) AS MONTH_NAME,
-    {{get_season('STARTED_AT')}} AS SEASON,
-    {{temporal_status('STARTED_AT')}} as TEMPORAL_STATUS
-    from {{ source('demo', 'bike') }}
-    where STARTED_AT != 'started_at'
-   
+    START_TIME AS STARTED_AT,
+    DATE(TO_TIMESTAMP(START_TIME)) as DATE_STARTED_AT,
+    DAYNAME(TO_TIMESTAMP(START_TIME)) as DAY_NAME,
+    {{day_type('START_TIME')}} as DAY_TYPE,
+    MONTHNAME(TO_TIMESTAMP(START_TIME)) AS MONTH_NAME,
+    {{get_season('START_TIME')}} AS SEASON,
+    {{temporal_status('START_TIME')}} as TEMPORAL_STATUS
+    from {{ source('demo', 'bike') }}   
 )
 
 select * from CTE
